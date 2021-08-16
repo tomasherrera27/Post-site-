336977b7d55290a076ac1759c302b148e1847e41
nico-limo <nicolas.limonoff@gmail.com>
2021-08-16 15:41:47 -0300

    done

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class PostItem extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor ejecutado");
+  }
+  componentDidMount() {
+    console.log("componentDidMount ejecutado");
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate ejecutado");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount ejecutado");
+  }
+  onDelete = e => {
+    e.preventDefault();
+    this.props.onDelete(this.props.post);
+  };
+  render() {
+    console.log("render ejecutado");
+    return (
+      <div>
+        <h1>{this.props.post.title}</h1>
+        <p>{this.props.post.content}</p>
+        <button onClick={this.onDelete}>Delete</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default PostItem;
